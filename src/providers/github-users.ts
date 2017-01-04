0f51420810744b803aa5f6b6bfe54331b2d81179
topdeveloper104 <foreverstar104@gmail.com>
2017-01-04 07:45:26 +0100

    first commit

---
+++ src/providers/github-users.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { User } from '../models/user';
+/*
+  Generated class for the GithubUsers provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class GithubUsers {
+  githubApiUrl = 'https://api.github.com';
+
+  constructor(public http: Http) {
+    console.log('Hello GithubUsers Provider');
+  }
+
+  load(): Observable<User[]>{
+    return this.http.get('https://api.github.com/users')
+    .map(res => <User[]>res.json());
+  }
+
+  loadDetails(login: string): Observable<User> {
+    return this.http.get('https://api.github.com/users/'+login)
+    .map(res => <User>res.json());
+  }
+
+  searchUsers(searchParam: string): Observable<User[]> {
+    return this.http.get('https://api.github.com/search/users?q=' + searchParam)
+    .map(res => <User[]>(res.json().items));
+  }
+}
